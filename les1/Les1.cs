fec059bb5b5623ada0db1bd8b1b855684faa830c
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2023-10-11 21:01:46 +0300

    Добавьте файлы проекта.

---
+++ les1/Les1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les1
+{
+    internal class Les1
+    {
+    }
+    internal class Task1
+    {
+        //Спроектируйте программу для построения генеалогического дерева.
+        //Учтите что у нас есть члены семьи у кого нет детей(дет). 
+        //Есть члены семьи у кого дети есть (взрослые). Есть мужчины и женщины.
+
+
+
+        //Доработать предыдущий класс реализовав методы вывода родителей, детей,
+        //братьев/сестер (включая двоюродных), бабушеки дедушек.
+        //Подумайте как лучше реализовать данные методы.
+
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public required Person ParentOne {  get; set; }
+        public required Person ParentTwo { get; set; }
+
+        public (Person,Person) GetParent()
+        { 
+            return (ParentOne, ParentTwo);
+        }
+
+        private List<Person> GetSiblings()
+        {
+            List<Person> listSiblings = new List<Person>();
+            listSiblings.AddRange(GetSiblingsByOneParents(ParentOne));
+            listSiblings.AddRange(GetSiblingsByOneParents(ParentTwo));
+
+            return listSiblings;
+        }
+
+            private List<Person> GetSiblingsByOneParents(Person Parent)
+        {
+            List<Person> listSiblings = new List<Person>();
+            if (Parent is PersonWithChildren)
+            {
+                foreach (var item in ((PersonWithChildren)Parent).GetChildren())
+                {
+                    if(this != item)
+                     listSiblings.Add(item);
+                }    
+            }
+            return listSiblings;
+        }
+
+
+    }
+
+    public class PersonWithChildren : Person 
+    { 
+        public required Person[] Children { get; set; }
+
+        public Person[]GetChildren()
+        {
+
+            return Children;
+        }
+    }
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+   
+}
