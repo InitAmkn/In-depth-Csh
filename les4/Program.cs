9e4c5fc8a97162f7726c1a418382e982bda86003
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2023-11-05 19:23:08 +0300

    les4 correction

--- les4/Program.cs
+++ les4/Program.cs
@@ -8,27 +8,39 @@ int[] ints = {30,20,7,0,1,7,6,2,1 };
 int num = 33;
 
 
-Console.WriteLine(isHaveSumThreeNumbers(num, ints));
+
+Console.WriteLine(string.Join(", ", searchForThreeNumbers(num, ints)));
 
 
-bool isHaveSumThreeNumbers(int num, int[] ints)
+int[] searchForThreeNumbers(int num, int[] ints)
 {
-    if(ints.Length<3) return false;
+    if(ints.Length<3) return new int[] { };
     
+
     for (int i = 0; i < ints.Length; i++)
     {
+        List<int> result = new List<int>();
         int numTemp = num - ints[i];
+    
         for (int j = i+1; j < ints.Length; j++)
         {
           int numTemp1 = numTemp - ints[j];
+          
             for (int k = j + 1; k < ints.Length; k++)
             {
                 int numTemp2 = numTemp1 - ints[k];
-                if (numTemp2 == 0) return true;
+                
+                if (numTemp2 == 0)
+                {    
+                    result.Add(ints[i]);
+                    result.Add(ints[j]);
+                    result.Add(ints[k]);
+                    return result.ToArray();
+                }
             }
         }
     }
-    return false;
+    return new int[] { };
 }
 
 
