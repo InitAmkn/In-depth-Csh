018f5f71aceabf054ad2e8b19e2f0ed9b0541502
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2023-12-03 20:42:45 +0300

    hw 8

---
+++ les8/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace les8
+{
+    class Program
+    {
+
+        //Объедините две предыдущих работы(практические работы 2 и 3): поиск файла и поиск текста в файле
+        //написав утилиту которая ищет файлы определенного расширения с указанным текстом.Рекурсивно.
+        //Пример вызова утилиты: utility.exe txt текст.
+
+     
+            static void Main(string[] args)
+            {
+            try
+            {
+                string directory = "D:\\Users\\mkn\\Desktop\\developer\\C#\\GB_projects\\les1\\";
+                string fileName = "text.txt";
+                string searchValue = "3333";
+
+                FileSearchUtility.SearchForFile(directory, fileName);
+                FileSearchUtility.SearchForValueInFile(fileName, searchValue);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+              
+            }
+
+        }
+    }
+
---
+++ web2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ThreadsSeminar
+{
+    internal class Program
+    {
+
+        //Напишите многопоточное приложение, которое определяет все
+        //IP-адреса интернет-ресурса и определяет до которого из них лучше Ping.
+
+        private static int _sum1 = 0;
+        private static int _sum2 = 0;
+
+        private static int[] _arr1 = { 1, 5, 8, 8, 7, 1, 7, 6, 4 };
+        private static int[] _arr2 = { 1, 9, 2, 3, 1, 4, 6, 4, 4 };
+
+        static void Main(string[] args)
+        {
+
+            Thread thread1 = new Thread(Task1);
+            Thread thread2 = new Thread(Task2);
+
+            thread1.Start();
+            thread2.Start();
+            thread1.Join(1000);
+
+            Console.WriteLine($"{_sum1} : {_sum2} : {_sum1 + _sum2}");
+
+        }
+
+        public static void Task1(object? arr)
+        {
+            _sum1 = _arr1.Sum();
+        }
+        public static void Task2(object? arr)
+        {
+            _sum2 = _arr2.Sum();
+        }
+
+
+    }
+
+
+
+
+
+
+}
