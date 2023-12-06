039e471d02de9ad7875d395aa3a6fb87a5baada3
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2023-12-06 22:08:08 +0300

    les9

---
+++ les9/Program.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
+
+namespace les9
+{
+      internal class Program
+        {
+        static void Main(string[] args)
+        {
+            string json = """{"Name": "Вася", "Surname": "Пупкин", "Age": 38}""";
+
+            var jsonObj = JsonConvert.DeserializeObject(json) as JObject;
+
+                XDocument xmlDoc = new XDocument(new XElement("Root", jsonObj.Properties().Select(p => new XElement(p.Name, p.Value))));
+                xmlDoc.Save("output.xml");
+            }
+        }
+    }
---
+++ web3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+
+namespace web3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string sait = "yandex.ru";
+
+            IPAddress[] iPAddress = Dns.GetHostAddresses(sait, System.Net.Sockets.AddressFamily.InterNetwork);
+
+            foreach (var item in iPAddress)
+            {
+                Console.WriteLine(item);
+            }
+
+            Dictionary<IPAddress, long> pings = new Dictionary<IPAddress, long>();
+
+            List<Thread> threads = new List<Thread>();
+            foreach (var item in iPAddress)
+            {
+                var thread1 = new Thread(() =>
+                {
+
+                    Ping p = new Ping();
+                    PingReply pingReply = p.Send(item);
+                    pings.Add(item, pingReply.RoundtripTime);
+                    Console.WriteLine($"{item} : {pingReply.RoundtripTime}");
+
+                });
+                threads.Add(thread1);
+                thread1.Start();
+
+            }
+
+            foreach (var item in threads)
+            {
+                item.Join();
+            }
+
+            long minPing = pings.Min(x => x.Value);
+
+            Console.WriteLine(minPing);
+
+
+        }
+    }
+}
